1f31ce25c272bc581e684bec25ba43dbb5166080
Eric Emmenegger <enemmenegger@gmail.com>
2025-09-29 04:57:09 -0400

    Bumped the robot speed back up

--- config.py
+++ config.py
@@ -119,7 +119,7 @@ w0_info = {
     'position': [0, 0],
     'rotation': 0,
     'visible': False,
-    'velocity': [0, 3],
+    'velocity': [0, 6],
     'ang_velocity': 0,
     'motors': [motors['m0'], motors['m1']],
     'motor_direction': [1, 1],
